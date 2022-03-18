ede9cb880bfd66bc62e81ee1fa84055136a7c49c
moojun <mgim32137@gmail.com>
2022-03-18 13:31:53 +0900

    Main.java 파일 생성

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+packege src;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        
+        System.out.println("hello");
+
+
+        
+    }
+
+}
